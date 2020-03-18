cd072d31b0e9a8b8aeed8cf5d0df8ca4dcc6f819
Aleksandr Kuzhelev <alexkuzh@gmail.com>
2020-03-18 14:24:28 -0400

    Appended Python solutions

---
+++ Python/8kyu/Remove String Spaces.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+'''Simple, remove the spaces from the string, then return the resultant string.'''
+def no_space(x):
+    return x.replace(' ','')
+
+print(no_space('8 j 8   mBliB8g  imjB8B8  jl  B'))
