2bb4643d503546ab37fd2a7328098cfe2cfdfb19
Aleksandr Kuzhelev <alexkuzh@gmail.com>
2020-03-23 22:26:28 -0400

    Appended Python solutions

--- Python/7kyu/Credit Card Mask.py
+++ Python/7kyu/Credit Card Mask.py
@@ -1,10 +1,6 @@
 # https://www.codewars.com/kata/5412509bd436bd33920011bc
 # return masked string
 def maskify(cc):
-    s = ''
-    for x in range(0, len(cc) - 4): s += "#"
-    if len(cc) > 4:
-        for x in cc[len(cc) - 4: len(cc)]: s += str(x)
-    else:
-        s = str(cc)
-    return s
+    return '#'*(len(cc)-4)+cc[-4:] if len(cc) > 3 else cc
+
+print(maskify('616'))
